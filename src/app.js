85466db652098f440fea1816597c99ca7bcb1a24
Georgiy Filippov <g.filippov777@gmail.com>
2023-11-29 10:39:35 +0500

    fix: remove useless code

--- src/app.js
+++ src/app.js
@@ -1,41 +1,31 @@
-import React, {useCallback} from 'react';
-import List from "./components/list";
-import Controls from "./components/controls";
-import Head from "./components/head";
-import PageLayout from "./components/page-layout";
+import PropTypes from 'prop-types';
+import React from 'react';
+import Head from './components/head';
+import List from './components/list';
+import PageLayout from './components/page-layout';
 
 /**
  * Приложение
- * @param store {Store} Хранилище состояния приложения
+ * @param {object} store  Хранилище состояния приложения
  * @returns {React.ReactElement}
  */
-function App({store}) {
-
+function App({ store }) {
   const list = store.getState().list;
-
-  const callbacks = {
-    onDeleteItem: useCallback((code) => {
-      store.deleteItem(code);
-    }, [store]),
-
-    onSelectItem: useCallback((code) => {
-      store.selectItem(code);
-    }, [store]),
-
-    onAddItem: useCallback(() => {
-      store.addItem();
-    }, [store])
-  }
 
   return (
     <PageLayout>
-      <Head title='Приложение на чистом JS'/>
-      <Controls onAdd={callbacks.onAddItem}/>
-      <List list={list}
-            onDeleteItem={callbacks.onDeleteItem}
-            onSelectItem={callbacks.onSelectItem}/>
+      <Head title='Магазин' />
+      <List list={list} />
     </PageLayout>
   );
 }
+
+App.propTypes = {
+  store: PropTypes.shape({
+    getState: PropTypes.func.isRequired,
+    setState: PropTypes.func.isRequired,
+    subscribe: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default App;
--- src/components/list/index.js
+++ src/components/list/index.js
@@ -1,33 +1,31 @@
-import React from "react";
 import PropTypes from 'prop-types';
-import Item from "../item";
+import React from 'react';
+import Item from '../item';
 import './style.css';
 
-function List({list, onDeleteItem, onSelectItem}) {
+function List({ list }) {
   return (
-    <div className='List'>{
-      list.map(item =>
-        <div key={item.code} className='List-item'>
-          <Item item={item} onDelete={onDeleteItem} onSelect={onSelectItem}/>
+    <div className='List'>
+      {list.map(item => (
+        <div
+          key={item.code}
+          className='List-item'
+        >
+          <Item item={item} />
         </div>
-      )}
+      ))}
     </div>
-  )
+  );
 }
 
 List.propTypes = {
-  list: PropTypes.arrayOf(PropTypes.shape({
-    code: PropTypes.number
-  })).isRequired,
-  onDeleteItem: PropTypes.func,
-  onSelectItem: PropTypes.func
+  list: PropTypes.arrayOf(
+    PropTypes.shape({
+      code: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      price: PropTypes.number.isRequired,
+    }),
+  ).isRequired,
 };
-
-List.defaultProps = {
-  onDeleteItem: () => {
-  },
-  onSelectItem: () => {
-  },
-}
 
 export default React.memo(List);
